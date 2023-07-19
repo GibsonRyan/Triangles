8af88bb5ea65c09666d7ee6dc49c32f4919b3ea0
Ryan Gibson <gibson.ryan2015@gmail.com>
2023-07-19 15:48:50 -0600

    More UI and Limiting to numeric

---
+++ Helpers/TextBoxHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using System.Text.RegularExpressions;
+
+namespace Triangles.Helpers
+{
+    public static class TextBoxHelper
+    {
+        public static readonly DependencyProperty IsNumericOnlyProperty =
+            DependencyProperty.RegisterAttached("IsNumericOnly", typeof(bool), typeof(TextBoxHelper), new UIPropertyMetadata(false, OnIsNumericOnlyChanged));
+
+        public static bool GetIsNumericOnly(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsNumericOnlyProperty);
+        }
+
+        public static void SetIsNumericOnly(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsNumericOnlyProperty, value);
+        }
+
+        private static void OnIsNumericOnlyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox)
+            {
+                TextBox textBox = (TextBox)d;
+                textBox.PreviewTextInput -= TextBox_PreviewTextInput;
+
+                if ((bool)e.NewValue)
+                {
+                    textBox.PreviewTextInput += TextBox_PreviewTextInput;
+                }
+            }
+        }
+
+        private static void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!IsTextAllowed(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private static bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+            return !regex.IsMatch(text);
+        }
+    }
+
+
+}
